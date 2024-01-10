5c34ac9bbb29142afdf05c949ee3f0d6e5a7fc69
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-01-10 13:19:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class RealTime():
     
-    def animatePlot(self, x, y,time=.1,plotter='scatter'):
+    def animatePlot(self, x, y,time=.1,plotter='scatter',color='black'):
         xplace = np.array([])
         yplace = np.array([])
 
@@ -13,13 +13,29 @@ class RealTime():
             xplace = np.append(xplace,x[z])
             yplace = np.append(yplace,y[z])
             if plotter == 'scatter':
-                plt.scatter(xplace, yplace)
+                plt.scatter(xplace, yplace, c=color)
             else:
-                plt.plot(xplace, yplace)
+                plt.plot(xplace, yplace, c=color)
             plt.draw()
             plt.pause(time)
             
         plt.show()
+
+    def animateHist(self, x, time=.1,color='black'):
+        xplace = np.array([])
+
+        for z in range(len(x)):
+            xplace = np.append(xplace,x[z])
+            plt.hist(xplace, color=color)
+            plt.draw()
+            plt.pause(time)
+            
+        plt.show()
+
+    
+    def animateRealTime():
+        pass
+
 
 
 x = np.arange(0, 10, 0.1)
@@ -28,4 +44,9 @@ y = np.sin(x)
 
 time = RealTime()
 
-time.animatePlot(x,y,plotter='plot')
+#time.animatePlot(x,y)
+
+
+x = np.random.normal(100, 20, 250)
+
+time.animateHist(x)
