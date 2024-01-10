bcb2b3fc805a0e87a03418e61559eee8a34ed3cc
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-01-10 12:50:17 -0700

    changes

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-class Maze():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+class RealTime():
+    
+    def animatePlot(self, x, y,time=.1,plotter='scatter'):
+        xplace = np.array([])
+        yplace = np.array([])
+
+        for z in range(len(x)):
+            xplace = np.append(xplace,x[z])
+            yplace = np.append(yplace,y[z])
+            if plotter == 'scatter':
+                plt.scatter(xplace, yplace)
+            else:
+                plt.plot(xplace, yplace)
+            plt.draw()
+            plt.pause(time)
+            
+        plt.show()
+
+
+x = np.arange(0, 10, 0.1)
+y = np.sin(x)
+
+
+time = RealTime()
+
+time.animatePlot(x,y,plotter='plot')
