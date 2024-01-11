feb75b5268fcf896d576cb2cd54cc0f5b3719ff8
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-01-11 01:47:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,40 @@ import timeit
 import matplotlib.pyplot as plt
 
 
-
 class RealTime():
+
     
+
     def animatePlot(self):
         print('ehys')
     
 
-    def realTimeComplex(self,stmt='pass', globals=globals(), number=5):
+    def realTimeComplex(self,stmt='pass', globals=globals(), value=40,number=1):
          
+        
+        result = timeit.repeat(stmt=stmt, globals=globals,repeat=value, number=number)
 
-        result = timeit.repeat(stmt=stmt, globals=globals, number=number)
-
-        x = np.arange(1,number+1)
+        
+        
+        x = np.arange(1,value+1)
         y = np.asarray(result)
-        print(x)
+        mean = np.mean(y)
+        print(mean)
+        bottom90 = np.percentile(y,90)
+        top10 = np.percentile(y,10)
+
 
         m, b = np.polyfit(x, y, 1)
-
-        plt.scatter(x,y)
-        plt.plot(x,m*x+b)
+        plt.scatter(x,y,zorder=0)
+        plt.plot(x,m*x+b,zorder=3,c='green')
+        plt.grid(zorder=-1.0)
+        plt.axhline(y=mean, zorder=4)
+        plt.axhline(y=bottom90,c='red')
+        plt.axhline(y=top10,c='purple')
+        plt.xlabel("Runs")
+        plt.ylabel("Time (ms)")
         plt.show()
+
 
     
     def animateRealTime():
@@ -32,14 +45,12 @@ class RealTime():
 
 
 
+
 def looper():
     for x in range(100):
-        print(x)
+        print(x*x)
 
-n=5
-
-print(globals())
 
 x = RealTime()
 
-x.realTimeComplex(stmt='looper()')
+x.realTimeComplex(stmt='looper()',value=500)
