e98e77e04a7d9f617f509ae996936988a1c77913
hodge-py <105604814+hodge-py@users.noreply.github.com>
2024-01-15 22:02:53 -0700

    changes

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
   name = 'SpaceTimeComplex',         # How you named your package folder (MyLib)
   packages = ['SpaceTimeComplex'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.3',      # Start with a small number and increase it with every change you make
   license='Copyright',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Big O calculations for Space And Time',   # Give a short description about your library
   author = 'Karson Hodge',                   # Type in your name
@@ -10,10 +15,12 @@ setup(
   url = 'https://github.com/hodge-py/SpaceTimeComplex',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/hodge-py/SpaceTimeComplex/archive/refs/tags/0.0.1.tar.gz',    # I explain this later on
   keywords = ['BigO', 'Algorithm', 'Calculations'],   # Keywords that define your package best
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   install_requires=[            # I get to this in a second
           'numpy',
           'pandas',
-          'sklearn-learn',
+          'scikit-learn',
           'matplotlib',
       ],
   classifiers=[
